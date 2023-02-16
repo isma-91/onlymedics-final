d8195562f71dcfa7fa5ea415de2c52dc5ea6ae78
isma-91 <isma91@hotmail.it>
2023-02-16 22:20:30 +0100

    add file names.php for array names for UserSeeder

---
+++ config/names.php
@@ -0,0 +1,83 @@
+<?php
+    return [
+        [
+            'name' => 'Antonio',
+            'last_name' => 'Rossi'
+        ],
+        [
+            'name' => 'Giovanni',
+            'last_name' => 'Trotta'
+        ],
+        [
+            'name' => 'Henri',
+            'last_name' => 'Kapidani'
+        ],
+        [
+            'name' => 'Simone',
+            'last_name' => 'Icardi'
+        ],
+        [
+            'name' => 'Marcantonio',
+            'last_name' => 'Bianchi'
+        ],
+        [
+            'name' => 'Adriano',
+            'last_name' => 'Grimaldi'
+        ],
+        [
+            'name' => 'Luca',
+            'last_name' => 'Lambiase'
+        ],
+        [
+            'name' => 'Gino',
+            'last_name' => 'Gigio'
+        ],
+        [
+            'name' => 'Christian',
+            'last_name' => 'De Sica'
+        ],
+        [
+            'name' => 'Emma',
+            'last_name' => 'Marrone'
+        ],
+        [
+            'name' => 'Lavinia',
+            'last_name' => 'Costa'
+        ],
+        [
+            'name' => 'Laura',
+            'last_name' => 'Pausini'
+        ],
+        [
+            'name' => 'Marco',
+            'last_name' => 'Baroncini'
+        ],
+        [
+            'name' => 'Nicola',
+            'last_name' => 'Bozzigoli'
+        ],
+        [
+            'name' => 'Federica',
+            'last_name' => 'Giordano'
+        ],
+        [
+            'name' => 'Ludovica',
+            'last_name' => 'Mancini'
+        ],
+        [
+            'name' => 'Valentina',
+            'last_name' => 'Rizzo'
+        ],
+        [
+            'name' => 'Francesco',
+            'last_name' => 'Oranzini'
+        ],
+        [
+            'name' => 'Lorenzo',
+            'last_name' => 'Palumbo'
+        ],
+        [
+            'name' => 'Vittorio',
+            'last_name' => 'Lo Preiato'
+        ]
+    ];
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,89 +15,90 @@ class UserSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $names = [
-            [
-                'name' => 'Antonio',
-                'last_name' => 'Rossi'
-            ],
-            [
-                'name' => 'Giovanni',
-                'last_name' => 'Trotta'
-            ],
-            [
-                'name' => 'Henri',
-                'last_name' => 'Kapidani'
-            ],
-            [
-                'name' => 'Simone',
-                'last_name' => 'Icardi'
-            ],
-            [
-                'name' => 'Marcantonio',
-                'last_name' => 'Bianchi'
-            ],
-            [
-                'name' => 'Adriano',
-                'last_name' => 'Grimaldi'
-            ],
-            [
-                'name' => 'Luca',
-                'last_name' => 'Lambiase'
-            ],
-            [
-                'name' => 'Gino',
-                'last_name' => 'Gigio'
-            ],
-            [
-                'name' => 'Christian',
-                'last_name' => 'De Sica'
-            ],
-            [
-                'name' => 'Emma',
-                'last_name' => 'Marrone'
-            ],
-            [
-                'name' => 'Lavinia',
-                'last_name' => 'Costa'
-            ],
-            [
-                'name' => 'Laura',
-                'last_name' => 'Pausini'
-            ],
-            [
-                'name' => 'Marco',
-                'last_name' => 'Baroncini'
-            ],
-            [
-                'name' => 'Nicola',
-                'last_name' => 'Bozzigoli'
-            ],
-            [
-                'name' => 'Federica',
-                'last_name' => 'Giordano'
-            ],
-            [
-                'name' => 'Ludovica',
-                'last_name' => 'Mancini'
-            ],
-            [
-                'name' => 'Valentina',
-                'last_name' => 'Rizzo'
-            ],
-            [
-                'name' => 'Francesco',
-                'last_name' => 'Oranzini'
-            ],
-            [
-                'name' => 'Lorenzo',
-                'last_name' => 'Palumbo'
-            ],
-            [
-                'name' => 'Vittorio',
-                'last_name' => 'Lo Preiato'
-            ]
-        ];
-
+        // Da capire dove mettere
+        // $names = [
+        //     [
+        //         'name' => 'Antonio',
+        //         'last_name' => 'Rossi'
+        //     ],
+        //     [
+        //         'name' => 'Giovanni',
+        //         'last_name' => 'Trotta'
+        //     ],
+        //     [
+        //         'name' => 'Henri',
+        //         'last_name' => 'Kapidani'
+        //     ],
+        //     [
+        //         'name' => 'Simone',
+        //         'last_name' => 'Icardi'
+        //     ],
+        //     [
+        //         'name' => 'Marcantonio',
+        //         'last_name' => 'Bianchi'
+        //     ],
+        //     [
+        //         'name' => 'Adriano',
+        //         'last_name' => 'Grimaldi'
+        //     ],
+        //     [
+        //         'name' => 'Luca',
+        //         'last_name' => 'Lambiase'
+        //     ],
+        //     [
+        //         'name' => 'Gino',
+        //         'last_name' => 'Gigio'
+        //     ],
+        //     [
+        //         'name' => 'Christian',
+        //         'last_name' => 'De Sica'
+        //     ],
+        //     [
+        //         'name' => 'Emma',
+        //         'last_name' => 'Marrone'
+        //     ],
+        //     [
+        //         'name' => 'Lavinia',
+        //         'last_name' => 'Costa'
+        //     ],
+        //     [
+        //         'name' => 'Laura',
+        //         'last_name' => 'Pausini'
+        //     ],
+        //     [
+        //         'name' => 'Marco',
+        //         'last_name' => 'Baroncini'
+        //     ],
+        //     [
+        //         'name' => 'Nicola',
+        //         'last_name' => 'Bozzigoli'
+        //     ],
+        //     [
+        //         'name' => 'Federica',
+        //         'last_name' => 'Giordano'
+        //     ],
+        //     [
+        //         'name' => 'Ludovica',
+        //         'last_name' => 'Mancini'
+        //     ],
+        //     [
+        //         'name' => 'Valentina',
+        //         'last_name' => 'Rizzo'
+        //     ],
+        //     [
+        //         'name' => 'Francesco',
+        //         'last_name' => 'Oranzini'
+        //     ],
+        //     [
+        //         'name' => 'Lorenzo',
+        //         'last_name' => 'Palumbo'
+        //     ],
+        //     [
+        //         'name' => 'Vittorio',
+        //         'last_name' => 'Lo Preiato'
+        //     ]
+        // ];
+        $names = config('names');
         $faker = Factory::create('it_IT');
 
         //Foreach per avere tutti i nomi differenti, ma da vedere se funzionerà quando faremo anche tutto il resto

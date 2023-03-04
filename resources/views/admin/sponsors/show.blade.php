6c6f6e62d43d271bf88743d60e0c699cd96ee591
isma-91 <isma91@hotmail.it>
2023-03-04 17:46:55 +0100

    add styles to title show payment

--- resources/views/admin/sponsors/show.blade.php
+++ resources/views/admin/sponsors/show.blade.php
@@ -12,7 +12,7 @@
         <h1 class="mb-5">Sponsorizza il tuo profilo così da avere innumerevoli vantaggi!!</h1>
         <h2 class="mb-5">{{$sponsor->title}}</h2>
         <p class="mb-5"><strong>{{ $sponsor->description }}</strong></p>
-        <p class="mb-5 text-light fs-1 fw-bold">{{ $sponsor->duration }} ORE DI SPONSORIZZAZIONE</p>
+        <p class="mb-5  fs-1 fw-bold premium-cta">{{ $sponsor->duration }} ORE DI SPONSORIZZAZIONE</p>
 
         <h2>Acquista adesso</h2>
         <a class="btn btn-primary text-light fw-bold" href="{{ route('admin.sponsors.payment', ['value' => $sponsor->title]) }}">{{ $sponsor->cost }}€</a>
